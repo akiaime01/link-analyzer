8c653e989e63beb78029d590cb3bac1e83880993
andrew-kotan <andrew.kotan@icloud.com>
2023-05-09 14:26:08 -0700

    crawl page recursion

--- crawl.js
+++ crawl.js
@@ -1,6 +1,22 @@
 const { JSDOM } = require('jsdom')
 
-async function crawlPage(currentURL) {
+async function crawlPage(baseURL, currentURL, pages) {
+
+    const baseURLObj = new URL(baseURL)
+    const currentURLObj = new URL(currentURL)
+
+    if (baseURLObj.hostname !== currentURLObj.hostname) {
+        return pages
+    }
+
+    const normalizedCurrentURL = normalizeURL(currentURL)
+    if (pages[normalizedCurrentURL] > 0) {
+        pages[normalizedCurrentURL]++
+        return pages
+    }
+
+    pages[normalizedCurrentURL] = 1
+
     console.log(`Actively Crawling ${currentURL}`)
 
     try {
@@ -14,10 +30,18 @@ async function crawlPage(currentURL) {
         const contentType = resp.headers.get("content-type")
         if (!contentType.includes("text/html")) {
             console.log(`non html response, content type: ${contentType} on page: ${currentURL}`)
-            return
+            return pages
         }
 
-        console.log(await resp.text())
+        const htmlBody = await resp.text()
+
+        const nextURLS = getURLsFromHTML(htmlBody, baseURL)
+
+        for (const nextURL of nextURLS) {
+            pages = await crawlPage(baseURL, nextURL, pages)
+        }
+        return pages
+
     } catch (err) {
         console.log(`error in fetch: ${err.message}, on page: ${currentURL}`)
     }
